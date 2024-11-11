42be900b693c1a37f6dbcf4f625b4429948538f8
PranjalBuilds <imcae22060@gmail.com>
2024-11-11 23:59:43 +0530

    Upload file UI

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
 const userRouter = require('./routes/user.routes');
-const connectToDb = require('./config/db');
+const indexRouter = require('./routes/index.routes');
 
 app.set('view engine', 'ejs');
 
+app.use('/', indexRouter);
 app.use('/user', userRouter);
-
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
